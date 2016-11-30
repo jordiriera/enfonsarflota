e0a1f718ebacf7505204755eb82f8ce8798ed400
jordiriera <jrierarayo@gmail.com>
2016-11-30 08:31:28 +0100

    Tauler "creat" + proves amb els vaixells

---
+++ src/enfonsarlaflota/Vaixell.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enfonsarlaflota;
+
+/**
+ *
+ * @author aluadmin
+ */
+public class Vaixell {
+    
+    enum tipusvaixell {portaavions, vaixell, llanxa}
+    
+    int vaixellenfonsat;        
+    int [][] posicionsOcupades ;
+    int vaixell;
+    boolean posicionsFerides = false;
+    String triarvaixell;
+    int longitud;
+        
+    
+    public void Vaixell(){
+        tipusvaixell.portaavions
+    }
+    
+    public void formaVaixells(){
+        if (triarvaixell.equals("portaavions")){
+            posicionsOcupades
+            
+        }
+    }
+    
+    public static void generarVaixells(int x, int y){
+        
+        
+        
+    }
+
+}
